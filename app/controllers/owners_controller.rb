776b2b6a1d9adec2a7e0cc4e0b1ad27aac36cbd5
github username <github email address>
2020-02-07 15:59:29 -0800

    added controller methods for owner, pets, notes

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,8 +1,20 @@
 class OwnersController < ApplicationController
 
+
 def show
+    owner = Owner.find_by(name: params[:id])
+    render :json =>  owners
 end 
-def new 
+
+def create 
+    owner = Owner.find_or_create_by(params.require(:owner).permit(:name, :address))
+    if owner.save
+        render :json =>  owner
+    else
+        flash[:message]=owner.errors.full_messages
+    end
 end 
+
+
 
 end
